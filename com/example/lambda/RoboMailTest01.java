aa48233c019b0271f7147666a0320a7267ce13b2
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2017-10-27 13:53:21 +0100

    Update lambda files
    Exercises 8-1:4

--- com/example/lambda/RoboMailTest01.java
+++ com/example/lambda/RoboMailTest01.java
@@ -1,11 +1,10 @@
 package com.example.lambda;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 /**
  * @author Oracle
- * Streams and filters
+ * Reuse lambda expressions
  */
 public class RoboMailTest01 {
   
@@ -13,31 +12,51 @@ public class RoboMailTest01 {
     
     List<Employee> pl = Employee.createShortList();
     RoboMail01 robo = new RoboMail01();
-
-    Predicate<Employee> salesExecutives = 
-      p -> p.getRole().equals(Role.EXECUTIVE) 
-        && p.getDept().equals("Sales");
     
-    Predicate<Employee> salesEmployeesOver50 = 
-      p -> p.getAge() >= 50 && p.getDept().equals("Sales");
+    //8-1
+    System.out.println("\n==== RoboMail 01");
+    System.out.println("\n=== Mail All Sales Over 50");
+    pl.stream()
+    	.filter(p -> p.getDept().equals("Sales"))
+    	.filter(p -> p.getAge() >= 50)
+    	.forEach(p -> robo.roboMail(p));
     
     System.out.println("\n==== RoboMail 01");
-    System.out.println("\n=== Sales Execs");
+    System.out.println("\n=== Text All Sales Over 50");
     pl.stream()
-        .filter(salesExecutives)
-        .forEach(p -> robo.roboMail(p));
+    	.filter(p -> p.getDept().equals("Sales"))
+    	.filter(p -> p.getAge() >= 50)
+    	.forEach(p -> robo.roboText(p));
     
-    pl.stream()
-        .filter(salesExecutives)
-        .forEach(p -> robo.roboText(p));    
+    System.out.println();
     
-    System.out.println("\n=== All Sales");
+    //8-2
+    System.out.println("\n==== RoboMail 01");
+    System.out.println("\n=== Sales Executives");
     pl.stream()
-      .filter(salesEmployeesOver50)
-      .forEach(p -> robo.roboMail(p));
+    	.filter(p -> p.getDept().equals("Sales"))
+    	.filter(p -> p.getRole().equals(Role.EXECUTIVE))
+    	.forEach(p -> robo.roboMail(p));
     
+    System.out.println();
+    
+    //8-3
+    System.out.println("\n==== RoboMail 01");
+    System.out.println("\n=== All Sales Over 50");
     pl.stream()
-        .filter(salesEmployeesOver50)
-        .forEach(p -> robo.roboText(p));
+    	.filter(p -> p.getDept().equals("Sales"))
+    	.filter(p -> p.getAge() >= 50)
+    	.forEach(p -> robo.roboMail(p));
+    
+    System.out.println();
+    
+    //8-4
+    System.out.println("\n==== RoboMail 01");
+    System.out.println("\n=== Male Eng under 65");
+    pl.stream()
+    	.filter(p -> p.getGender().equals(Gender.MALE))
+    	.filter(p -> p.getDept().equals("Eng"))
+    	.filter(p -> p.getAge() <= 65)
+    	.forEach(p -> robo.roboMail(p));
   }
 }
