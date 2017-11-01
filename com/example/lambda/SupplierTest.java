75f1583deee56e5c140422410e625825fff550da
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2017-11-01 13:44:49 +0000

    Add files via upload

--- com/example/lambda/SupplierTest.java
+++ com/example/lambda/SupplierTest.java
@@ -15,17 +15,30 @@ public class SupplierTest {
     List<Employee> eList = Employee.createShortList();
     Employee first = eList.get(0);
     
-    Supplier<Employee> eSupp = e ->
-    		System.out.println("Given name: " + e.getGivenName() + "\nSurName: " + e.getSurName()
-    		+ "\nAge: " + e.getAge() + "\nGender: " + e.getGender() + "\nRole: " + e.getRole()
-    		+ "\nDEPT: " + e.getDept() + "\nStart Date: " + e.getStartDate() + "\nSalary: " + e.getSalary()
-    		+ "\nEmail: " + e.getEmail() + "\nPhoneNumber: " + e.getPhone() + "\nAddress: " + e.getAddress()
-    		+ "\nCity: " + e.getCity() + "\nState: " + e.getState() + "\nCode: " + e.getCode());
-      
+    Supplier<Employee> eSupp =
+    		() -> new Employee.Builder()
+    		.givenName("Jill")
+    		.surName("Doe")
+    		.age(26)
+    		.gender(Gender.FEMALE)
+    		.role(Role.STAFF)
+    		.dept("Sales")
+    		.startDate(LocalDate.of(2012, 7, 14))
+    		.salary(45000)
+    		.email("jill.doe@example.com")
+    		.phoneNumber("202-123-4678")
+    		.address("33 3rd St")
+    		.city("Smallville")
+    		.state("KS")
+    		.code("12333")
+    		.build();
+    		
+    		
     System.out.println("=== Print employee list before");
     eList.stream().forEach(Employee::printSummary);
     
     // Add new employee here
+    eList.add(eSupp.get());
     
     System.out.println("\n=== Print employee list after");
     eList.stream().forEach(Employee::printSummary);
